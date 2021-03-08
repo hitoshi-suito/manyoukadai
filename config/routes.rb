5c7fc07e5bc47a431fc07c38b7f47b4cb7cf8495
hitoshi-suito <htshts153@gmail.com>
2021-03-08 22:53:19 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/show'
-  get 'tasks/edit'
-  get 'tasks/destroy'
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
