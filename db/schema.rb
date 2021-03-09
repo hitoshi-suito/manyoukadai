3f0af6ae6a822c2d9cf39ef4d49f1c6e94cab094
hitoshi-suito <htshts153@gmail.com>
2021-03-09 19:41:01 +0900

    User,Labelモデル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,33 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_031248) do
+ActiveRecord::Schema.define(version: 2021_03_09_030340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "labels", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "task_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "details", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "tasks", "users"
 end
