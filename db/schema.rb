4006b8d400b269b2749908467ee04920390b488a
hitoshi-suito <htshts153@gmail.com>
2021-03-09 12:26:54 +0900

    tasksテーブルのマイグレーションファイルにnot null制約記載

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_031248) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "details"
+    t.string "details", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
